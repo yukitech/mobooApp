d87581f44f251ee748266b6e97a27563bdc44530
yukitech <yuki838ikrkj@gmail.com>
2024-01-19 12:19:27 +0900

    app(api) dockerの作成

---
+++ app/api/csi_predict_api.py
@@ -0,0 +1,67 @@
+from fastapi import FastAPI, File, UploadFile, Depends
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+import uvicorn
+import shutil
+import tempfile
+
+from predict.predict_comp import predict
+import crud, models, schemas
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+origins = [
+    "http://localshot",
+    "http://localhost:3000",
+    "http://133.130.109.157",
+    "http://133.130.109.157:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get('/')
+def index():
+    return("Hello csi predict api")
+
+@app.post('/predict')
+def save_predict(db: Session = Depends(get_db), file: UploadFile = File(...)):
+    with tempfile.NamedTemporaryFile(delete=True, dir=".", suffix=".csv") as temp_file:
+        shutil.copyfileobj(file.file, temp_file)
+        pred_result, prob_result = predict(temp_file.name, file.filename)
+
+        result = schemas.PredictResultCreate(file_name=file.filename, pred_result=pred_result, prob_result=prob_result)
+        crud.save_predict_result(db=db,result=result)
+
+    return JSONResponse(content={"res": "ok", "filename": file.filename})
+
+@app.get('/get_pred_result')
+def get_pred_result(db: Session = Depends(get_db)):
+    return crud.get_predict_result(db=db)
+
+@app.get('/get_prob_result/{id}')
+def get_prob_result(id, db: Session = Depends(get_db)):
+    return crud.get_prob_result(id=id, db=db)
+
+@app.get('/all')
+def get_all(db: Session = Depends(get_db)):
+    return crud.get_all(db=db)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ app/api/predict/predict_comp.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+from tensorflow import keras
+import numpy as np
+
+from .module.read_csv import read_file
+from .module.csi_data_process import CsiDataProcess
+
+# constant definition
+MODEL = 'predict/model/LSTM_model.hdf5'
+MAX_TIMESTEP = 280
+CLASS_NUM = 4
+
+def pre_process(original_data, division_num):
+  process = CsiDataProcess(int(MAX_TIMESTEP/division_num))
+  processed_data = process.get_data(original_data)
+
+  all_action_data = processed_data["phase_by_point"]
+  data = []
+  for action_data in all_action_data:
+    for point_group in action_data:
+      data.append(point_group)
+
+  data = np.array(data, dtype=float)
+
+  return data
+
+def predict(csi_data_file, file_name):
+  predict_original_data = read_file(csi_data_file, file_name)
+  predict_data = pre_process(predict_original_data, 1)
+  model = keras.models.load_model(MODEL)
+
+  probs = model.predict(predict_data, batch_size=1)
+
+  ans = {"sitdown":0, "standup":0, "fall":0, "walk":0}
+  for prob, dict_key in zip(probs[0], ans.keys()):
+    ans[dict_key] = str(prob)
+  
+  sorted_ans = sorted(ans.items(), key=lambda x:x[1], reverse=True)
+
+  sorted_ans = dict(sorted_ans)
+
+  pred_result = next(iter(sorted_ans))
+  prob_result = str(sorted_ans)
+
+  return pred_result, prob_result
